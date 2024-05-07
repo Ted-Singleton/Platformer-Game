d1ffe4f4eccd9a0a4ab05176fa896b13b4a5c35c
Ted Singleton <tedsingleton6@gmail.com>
2024-05-07 16:48:32 +0100

    Reworked database insertion logic so only best times are added, and removed GetLevelID method since that functionality is covered by LevelManager

--- Ted-Singleton-Game/Assets/Scripts/Gameplay/Level/Level Complete/LevelCompleteScreen.cs
+++ Ted-Singleton-Game/Assets/Scripts/Gameplay/Level/Level Complete/LevelCompleteScreen.cs
@@ -27,17 +27,14 @@ public class LevelCompleteScreen : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        Debug.Log(this.ToString());
         //get the current scene name
         levelName = LevelData.levelName;
         //set the text to the current scene name
-        Debug.Log(levelName);
         levelNameText = GameObject.Find("LevelName").GetComponent<TextMeshProUGUI>();
         levelNameText.text = levelName;
 
         levelTime = LevelData.time;
         //set the time text to the time taken to complete the level
-        Debug.Log(levelTime);
         levelTimeText = GameObject.Find("LevelTime").GetComponent<TextMeshProUGUI>();
         levelTimeText.text = "Time: " + LevelData.time.ToString("F2") + "s";
 
@@ -48,31 +45,29 @@ public class LevelCompleteScreen : MonoBehaviour
         collectibleText.text = collectibleGathered ? "Collected!" : "Not Collected";
 
         //we use the database manager to save the level data to the database
-        /* As a note, the intent here is to only save the player's time to the database, not whether they gathered the collectible
-           the collectible is meant as a personal challenge for the player, and not a requirement to complete the level and compete with other players*/
-        string query = $"INSERT INTO PlayerTimes(LevelID, PlayerID, CompletionTime) VALUES ('{levelName}', '{0}', '{levelTime}')";
-        DatabaseManager.ExecuteQuery(query);
+
+        /* NOTE: we're using an ID of 0 here, as we don't have a player ID system in place yet
+        in a full game, we would most likely use something like the player's Steam ID or some other unique account identifier
+        this would allow us to track the player's progress across multiple devices, as well as allow for the display of profile pictures and other player-specific data*/
+        DatabaseManager.WriteTimeToDatabase(levelName, 0, levelTime);
+
+        //now we need to save the level completion status to the local save file
+        SaveToFile();
     }
 
-    //we can't guarantee that every script is ready before the start method is called, so we use the update method to save the level completion status
-    private void Update()
+    //update: we don't need to use Update() for this part, as you can set the load order of scripts in Project Settings -> Script Execution Order
+    //so, we CAN guarantee that the LevelManager and SaveManager will be ready before this script runs, and we can use FindObjectOfType to get them
+    private void SaveToFile()
     {
-        //if we haven't already saved the level completion status, we attempt to
-        if (!saved)
-        {
-            //we need the level manager to get the level ID
-            LevelManager levelManager = FindObjectOfType<LevelManager>();
-            int levelID = levelManager.GetLevelID(levelName);
+        //we need the level manager to get the level ID
+        LevelManager levelManager = FindObjectOfType<LevelManager>();
+        int levelID = levelManager.GetLevelID(levelName);
 
-            //if the level ID is -1, we just return, and the GetLevelID call will have logged an error
-            if (levelID == -1) return;
+        //if the level ID is -1, we just return, and the GetLevelID call will have logged an error
+        if (levelID == -1) return;
 
-            //we use the save manager to save the level completion status
-            SaveManager saveManager = FindObjectOfType<SaveManager>();
-            saveManager.CompleteLevel(levelID, collectibleGathered);
-
-            //we set the saved flag to true so we don't save the level completion status again
-            saved = true;
-        }
+        //we use the save manager to save the level completion status
+        SaveManager saveManager = FindObjectOfType<SaveManager>();
+        saveManager.CompleteLevel(levelID, collectibleGathered);
     }
 }
--- Ted-Singleton-Game/Assets/Scripts/Utility/DatabaseManager.cs
+++ Ted-Singleton-Game/Assets/Scripts/Utility/DatabaseManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
+    //used for running queries that don't return data
     public static void ExecuteQuery(string query)
     {
         string connectionString = "Server=localhost,1433;Database=GameDB;User Id=SA;Password=Password1!;";
@@ -15,27 +16,52 @@ public class DatabaseManager : MonoBehaviour
             {
                 command.ExecuteNonQuery();
             }
+            connection.Close();
         }
     }
 
-    public static int GetLevelID(string levelName)
+    public static void WriteTimeToDatabase(string levelName, int playerID, float levelTime)
     {
+        //we use the database manager to save the level completion data to the database
+        /* As a note, the intent here is to only save the player's time to the database, not whether they gathered the collectible
+           the collectible is meant as a personal challenge for the player, and not a requirement to complete the level and compete with other players*/
+
         string connectionString = "Server=localhost,1433;Database=GameDB;User Id=SA;Password=Password1!;";
+
+        //we'll need to run a query to check if the player has already completed the level, and what their currently saved time was
+        string checkBestQuery = $"SELECT CompletionTime FROM PlayerTimes WHERE LevelID = '{levelName}' AND PlayerID = '{playerID}'";
+
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
             connection.Open();
-            string query = $"SELECT LevelID FROM Levels WHERE DisplayName = '{levelName}'";
-            using (SqlCommand command = new SqlCommand(query, connection))
+
+            //check the current time for the level and player
+            using (SqlCommand commandCheckTime = new SqlCommand(checkBestQuery, connection))
             {
-                using (SqlDataReader reader = command.ExecuteReader())
+                SqlDataReader reader = commandCheckTime.ExecuteReader();
+
+                //if the query returns a result, we check if the new time is better than the current time
+                if (reader.Read())
                 {
-                    if (reader.Read())
+                    //get the current time from the database
+                    //while the data type in the database is a float, double is more compatible here, so we use that
+                    double currentTime = reader.GetDouble(0);
+
+                    //check if the new time is better than the current time
+                    if (levelTime < currentTime || currentTime == 0)
                     {
-                        return reader.GetInt32(0);
+                        //if the new time is better, update the time in the database
+                        string queryUpdate = $"UPDATE PlayerTimes SET CompletionTime = '{levelTime}' WHERE LevelID = '{levelName}' AND PlayerID = '{playerID}'";
+                        ExecuteQuery(queryUpdate);
                     }
+                }
+                else
+                {
+                    //if there is no time recorded, we insert the new time
+                    string queryInsert = $"INSERT INTO PlayerTimes(LevelID, PlayerID, CompletionTime) VALUES ('{levelName}', '{playerID}', '{levelTime}')";
+                    ExecuteQuery(queryInsert);
                 }
             }
         }
-        return -1;
     }
 }
